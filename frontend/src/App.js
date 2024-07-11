3297b5cc207b4ca497a67a71f6239a93ebcd8d37
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2024-07-11 17:28:20 +0530

    implemented search flight feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./Pages/Auth/Login";
 import Register from "./Pages/Auth/Register";
+
 import Home from "./Pages/Home";
+import FlightSearch from "./Pages/Flight/FlightSearch";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<FlightSearch />} />
       </Routes>
     </>
   );
